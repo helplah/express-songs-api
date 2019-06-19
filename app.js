1620518a520d92da528a900282d0f19b5fee674a
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-19 12:19:00 +0800

    Refactor code to use app.param()

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 let songs = [];
 
+app.param("id", (req, res, next, id) => {
+  req.song = songs.find(song => {
+    return song.id == id;
+  });
+
+  next();
+});
+
 //return list of all songs
-app.get('/songs', (req, res) => {
+app.get("/songs", (req, res) => {
   res.status(200).json(songs);
 });
 
 //create a new song, and return new song
-app.post('/songs', (req, res) => {
+app.post("/songs", (req, res) => {
   let newSong = {
     id: songs.length + 1,
     name: req.body.name,
-    artist: req.body.artist 
-  }
+    artist: req.body.artist
+  };
   songs.push(newSong);
   res.status(201).json(newSong);
 });
 
-//return a song with id 
-app.get('/songs/:id', (req, res) => {
-  let song = songs.find(song => song.id == parseInt(req.params.id));
+//return a song with id
+app.get("/songs/:id", (req, res) => {
+  const song = req.song;
   res.status(200).json(song);
 });
 
 //edit a song with id, and return edited song
-app.put('/songs/:id', (req, res) => {
-  let song = songs.find(song => song.id === parseInt(req.params.id));
+app.put("/songs/:id", (req, res) => {
+  const song = req.song;
   song.name = req.body.name;
   song.artist = req.body.artist;
   res.status(200).json(song);
@@ -37,10 +45,10 @@ app.put('/songs/:id', (req, res) => {
 
 //delete a song with id, and return deleted song
 app.delete("/songs/:id", (req, res) => {
-  let songToDelete = songs.find(song => song.id === parseInt(req.params.id));
+  const songToDelete = req.song;
   let index = songs.indexOf(songToDelete);
   songs.splice(index, 1);
   res.status(200).json(songToDelete);
 });
 
-module.exports = app
+module.exports = app;
